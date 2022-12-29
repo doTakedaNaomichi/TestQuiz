f8c86a3a8e3c77061ddb553e36faff82305e2926
doTakedaNaomichi <115389287+doTakedaNaomichi@users.noreply.github.com>
2022-12-29 14:12:39 +0900

    desctop5

---
+++ web-desktop-004/cocos-js/wait-for-ammo-instantiation.js
@@ -0,0 +1,2 @@
+System.register(["./instantiated-945fca5d.js"],(function(t){"use strict";return{setters:[function(e){t("default",e.h_)}],execute:function(){}}}));
+//# sourceMappingURL=wait-for-ammo-instantiation.js.map
